3b17c06558ecab11ef2efcd7e5770b1df8e79225
Ivan Krsnik <ivankrsniksa@gmail.com>
2020-01-21 10:57:04 +0100

    GridView, GridItem, changed home page source

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,16 +1,34 @@
 import 'package:flutter/material.dart';
 
+import './dummy-data.dart';
+import './category_item.dart';
+
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(children: <Widget>[
-      
-      // grid delegate takes care of structuring the grid, must be called
-    ], gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-      // each grid item will take 200 px
-      maxCrossAxisExtent: 200,
-      childAspectRatio: 3 / 2, // dynamic width / height
-      mainAxisSpacing: 20. // spacing between grid items
-    ));
+    // every page should return Scaffold widget
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeal'),
+      ),
+      body: GridView(
+        children: DUMMY_CATEGORIES
+            .map(
+              (categoryData) => CategoryItem(
+                categoryData.title,
+                categoryData.color,
+              ),
+            )
+            .toList(),
+        // grid delegate takes care of structuring the grid, must be called
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          // each grid item will take 200 px
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2, // dynamic width / height
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20, // spacing between grid items
+        ),
+      ),
+    );
   }
 }
