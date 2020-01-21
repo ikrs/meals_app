4798985e9ff4f9c7814d044181002e141785fe4d
Ivan Krsnik <ivankrsniksa@gmail.com>
2020-01-21 21:50:45 +0100

    adding filter logic, ALL ITERABLE MUST AT THE END USE .toList()

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,49 @@ import './screens/categories_screen.dart';
 import './screens/category_meals_screen.dart';
 import './screens/tabs_screen.dart';
 import './screens/filters_screen.dart';
+import './dummy-data.dart';
+import './models/meal.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Map<String, bool> _filters = {
+    'gluten': false,
+    'lactose': false,
+    'vegan': false,
+    'vegetarian': false,
+  };
+  List<Meal> _availableMeals = DUMMY_MEALS;
+
+  void _setFilters(Map<String, bool> filterData) {
+    setState(() {
+      _filters = filterData;
+
+      // EVERY ITERABLE MUST HAVE .toList()
+      _availableMeals = DUMMY_MEALS.where((meal) {
+        // return false if we dont want to include item
+        if (_filters['gluten'] && !meal.isGlutenFree) {
+          return false;
+        }
+        if (_filters['lactose'] && !meal.isLactoseFree) {
+          return false;
+        }
+        if (_filters['vegan'] && !meal.isVegan) {
+          return false;
+        }
+        if (_filters['vegetarian'] && !meal.isVegetarian) {
+          return false;
+        }
+        return true;
+      }).toList();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -35,9 +74,9 @@ class MyApp extends StatelessWidget {
       home: TabsScreen(),//CategoriesScreen(),
       // defining named routes
       routes: {
-        CategoryMealsScreen.routeName: (context) => CategoryMealsScreen(),
+        CategoryMealsScreen.routeName: (context) => CategoryMealsScreen(_availableMeals),
         MealDetailScreen.routeName: (context) => MealDetailScreen(),
-        FiltersScreen.routeName: (context) => FiltersScreen(),
+        FiltersScreen.routeName: (context) => FiltersScreen(_filters, _setFilters),
       },
       // triggers if route cant be found or for highly dynamic apps
       onGenerateRoute: (settings) {
