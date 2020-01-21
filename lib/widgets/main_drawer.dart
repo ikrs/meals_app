1f10d1f420f2a7145c87c35e3a8c983a4d5d109b
Ivan Krsnik <ivankrsniksa@gmail.com>
2020-01-21 19:52:35 +0100

    replacing pages insted of pushing

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -47,10 +47,11 @@ class MainDrawer extends StatelessWidget {
             height: 20,
           ),
           buildListTile('Meals', Icons.restaurant,() {
-            Navigator.of(context).pushNamed('/');
+            // replace existing place with this page
+            Navigator.of(context).pushReplacementNamed('/');
           }),
           buildListTile('Filters', Icons.settings, () {
-            Navigator.of(context).pushNamed(FiltersScreen.routeName);
+            Navigator.of(context).pushReplacementNamed(FiltersScreen.routeName);
           }),   
         ],
       ),
