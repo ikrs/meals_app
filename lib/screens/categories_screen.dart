9c43bc949fe17aed87f29ced83f208fce2bbfe01
Ivan Krsnik <ivankrsniksa@gmail.com>
2020-01-21 17:57:06 +0100

    adding tab bar

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -6,31 +6,25 @@ import '../widgets/category_item.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // every page should return Scaffold widget
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('DeliMeal'),
-      ),
-      body: GridView(
-        padding: const EdgeInsets.all(25),
-        // map each dummy data to CategoryItem
-        children: DUMMY_CATEGORIES
-            .map(
-              (categoryData) => CategoryItem(
-                categoryData.id,
-                categoryData.title,
-                categoryData.color,
-              ),
-            )
-            .toList(),
-        // grid delegate takes care of structuring the grid, must be called
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          // each grid item will take 200 px
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2, // dynamic width / height
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20, // spacing between grid items
-        ),
+    return GridView(
+      padding: const EdgeInsets.all(25),
+      // map each dummy data to CategoryItem
+      children: DUMMY_CATEGORIES
+          .map(
+            (categoryData) => CategoryItem(
+              categoryData.id,
+              categoryData.title,
+              categoryData.color,
+            ),
+          )
+          .toList(),
+      // grid delegate takes care of structuring the grid, must be called
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        // each grid item will take 200 px
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2, // dynamic width / height
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20, // spacing between grid items
       ),
     );
   }
---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import './categories_screen.dart';
+import './favorites_screen.dart';
+
+class TabsScreen extends StatefulWidget {
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      initialIndex: 0,// default tab selected
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Meals'),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.category),
+                text: 'Categories',
+              ),
+              Tab(
+                icon: Icon(Icons.star),
+                text: 'Favorites',
+              ),
+            ],
+          ),
+        ),
+        // here we must add same amount of child widgets as number of Tab()
+        // widgets created above, so in our case its 2
+        body: TabBarView(
+          children: <Widget>[
+            CategoriesScreen(),
+            FavoritesScreen(),
+          ],
+        ),
+      ),
+    );
+  }
+}
