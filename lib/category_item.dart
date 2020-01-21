d451314c3e5eee37ae46c98ec33d589ec0e259e4
Ivan Krsnik <ivankrsniksa@gmail.com>
2020-01-21 11:37:05 +0100

    added new page, Navigator

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,26 +1,48 @@
 import 'package:flutter/material.dart';
 
+import './category_meals_screen.dart';
+
 class CategoryItem extends StatelessWidget {
   final String title;
   final Color color;
 
   CategoryItem(this.title, this.color);
 
+  void selectCategory(BuildContext context) {
+    // adding context for Navigator to know what a current screen/page is
+    // push a new page
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (_) {
+        return CategoryMealsScreen();
+      }),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Text(title, style: Theme.of(context).textTheme.title),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            color.withOpacity(0.7),
-            color,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    // adding onTap via InkWell witch is basically a GestureDetector
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      // this borderRadius should match borderRadius of parent, in this case a Container
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.title,
         ),
-        borderRadius: BorderRadius.circular(15),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(0.7),
+              color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
       ),
     );
   }
